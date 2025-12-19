54c7d79b3c807eea10a38758984b367872ffe5df
Wulu <wulu@wulu.zone>
2025-12-19 18:33:01 +0800

    feat: initial commit for ESP32-C3-Zero-Basic-Kit

---
+++ MicroPython/01-Hello-ESP32/hello_esp32.py
@@ -0,0 +1,13 @@
+"""
+Hello ESP32
+
+Description: Prints 'Hello, ESP32!' to the console.
+
+Wiring:
+- ESP32-C3-Zero
+
+Link:
+- Tutorial [ZH]: https://docs.waveshare.net/ESP32-MicroPython-Tutorials/Getting-Started
+"""
+
+print('Hello, ESP32!')
